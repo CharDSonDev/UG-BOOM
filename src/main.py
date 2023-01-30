17f8c376f7e890b92b135f184757ee39d6a90314
CharDSonDev <chardinson05082002@gmail.com>
2023-01-30 17:47:50 -0400

    update

---
+++ src/main.py
@@ -0,0 +1,105 @@
+# main.py
+#
+# Copyright 2023 Chardinson
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import sys
+import gi
+import socket
+
+gi.require_version('Gtk', '4.0')
+gi.require_version('Adw', '1')
+
+from gi.repository import Gtk, Gio, Adw
+from .window import UgBoomWindow
+
+UE_BOOM_MAC = "C0:28:8D:8F:C3:6F"
+
+UE_BOOM_PORT = 1
+
+UE_BOOM_OFF_MSG = b'\x02\x01\xb6'
+
+
+class Ug_boomApplication(Adw.Application):
+    """The main application singleton class."""
+
+    def __init__(self):
+        super().__init__(application_id='com.chardson.ug_boom',
+                         flags=Gio.ApplicationFlags.FLAGS_NONE)
+        self.create_action('quit', self.quit, ['<primary>q'])
+        self.create_action('about', self.on_about_action)
+        self.create_action('preferences', self.on_preferences_action)
+        self.create_action('shutdown', self.on_shutdown_action)
+
+    def do_activate(self):
+        """Called when the application is activated.
+
+        We raise the application's main window, creating it if
+        necessary.
+        """
+        win = self.props.active_window
+        if not win:
+            win = UgBoomWindow(application=self)
+        win.present()
+
+    def on_about_action(self, widget, _):
+        """Callback for the app.about action."""
+        about = Adw.AboutWindow(transient_for=self.props.active_window,
+                                application_name='UG BOOM',
+                                application_icon='com.chardson.ug_boom',
+                                developer_name='Chardinson',
+                                version='0.1.0',
+                                developers=['Chardinson'],
+                                copyright='© 2023 Chardinson')
+        about.present()
+
+    def on_preferences_action(self, widget, _):
+        """Callback for the app.preferences action."""
+        print('app.preferences action activated')
+
+    def on_shutdown_action(self, widget, _):
+        dev = socket.socket(
+            socket.AF_BLUETOOTH,
+            socket.SOCK_STREAM,
+            socket.BTPROTO_RFCOMM
+        )
+        dev.connect((
+            UE_BOOM_MAC,
+            UE_BOOM_PORT
+        ))
+        dev.sendall(UE_BOOM_OFF_MSG)
+        dev.close()
+
+    def create_action(self, name, callback, shortcuts=None):
+        """Add an application action.
+
+        Args:
+            name: the name of the action
+            callback: the function to be called when the action is
+              activated
+            shortcuts: an optional list of accelerators
+        """
+        action = Gio.SimpleAction.new(name, None)
+        action.connect("activate", callback)
+        self.add_action(action)
+        if shortcuts:
+            self.set_accels_for_action(f"app.{name}", shortcuts)
+
+def main(version):
+    """The application's entry point."""
+    app = Ug_boomApplication()
+    return app.run(sys.argv)
